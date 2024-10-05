b26ae04073f981b591e089fb4f626f04337c3315
YuichiKamishiro <yuichikamishiro@gmail.com>
2024-10-05 10:26:28 -0400

    speed

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <thread>
 #include <vector>
 #include <iostream>
+#include <ctime>
 
 using namespace sf; 
 
@@ -15,7 +16,7 @@ const int BOIDS_COUNT = 30;
 // in pixels
 const int MAX_RANGE = 40;
 
-const float TURNFACTOR = 0;
+const float TURNFACTOR = 0.2;
 const float COHESION_FACTOR = 0.0005;
 
 struct Boid {
@@ -26,7 +27,7 @@ struct Boid {
 
     Boid(int x, int y, int vx, int vy): 
         x(x), y(y), vx(vx), vy(vy),
-        shape(10)
+        shape(5)
     {
         shape.setFillColor(Color::Red);
         shape.setPosition(x, y);
@@ -53,6 +54,7 @@ void checkBorders(Boid &boid) {
 
 
 int main() {
+    srand(time(NULL));
 
     std::array<Boid, BOIDS_COUNT> boids;
     spawnBoids(boids);
@@ -73,49 +75,70 @@ int main() {
             int avg_x = 0; int avg_y = 0;
             int avg_vx = 0; int avg_vy = 0;
             int flock_boids = 0;
+            int close_dx = 0;
+            int close_dy = 0;
 
             for (int j = 0; j < BOIDS_COUNT; j++) {
                 if (i != j) {
                     auto &otherBoid = boids[j];
-
                     int distance_x = boid.x - otherBoid.x;
                     int distance_y = boid.y - otherBoid.y;
-                    if (distance_x < MAX_RANGE && distance_y < MAX_RANGE) {
-                        // distance
-                        if (distance_x != 0 && distance_y != 0) {
-                            boid.vx -= (otherBoid.x - boid.x) / distance_x;
-                            boid.vy -= (otherBoid.y - boid.y) / distance_y;
-                        }
-                        // pos
-                        avg_x += otherBoid.x;
-                        avg_y += otherBoid.y;
-                        // vel
-                        avg_vx += otherBoid.vx;
-                        avg_vy += otherBoid.vy;
 
-                        flock_boids += 1;
+                    if (distance_x < MAX_RANGE && distance_y < MAX_RANGE) {
+                        int sqr_d = distance_x*distance_x + distance_y*distance_y;
+
+                        if (sqr_d < 8*8) {                            
+                            // distance
+                            close_dx += boid.x - otherBoid.x;
+                            close_dy += boid.y - otherBoid.y;
+                        } else if(sqr_d < MAX_RANGE*MAX_RANGE){
+                            // pos
+                            avg_x += otherBoid.x;
+                            avg_y += otherBoid.y;
+                            // vel
+                            avg_vx += otherBoid.vx;
+                            avg_vy += otherBoid.vy;
+
+                            flock_boids += 1;
+                        }
 
                     }
                 }
             }
             if (flock_boids > 0) {
-                boid.vx += (avg_x / flock_boids - boid.x);
-                boid.vy += (avg_y / flock_boids - boid.y);
-                boid.vx += (avg_vx / flock_boids - boid.vx);
-                boid.vy += (avg_vy / flock_boids - boid.vy);
+                avg_x /= flock_boids;
+                avg_y /= flock_boids;
+                avg_vx /= flock_boids;
+                avg_vy /= flock_boids;
+
+                boid.vx = (boid.vx + 
+                   (avg_x - boid.x) * 0.0005 + 
+                   (avg_vx - boid.vx)* 0.05);
+
+                boid.vy = (boid.vy + 
+                   (avg_y - boid.y)*0.0005 + 
+                   (avg_vy - boid.vy)*0.05);
+
             }
+            boid.vx = boid.vx + (close_dx*0.05);
+            boid.vy = boid.vy + (close_dy*0.05);
 
-            boid.x += boid.vx;
-            boid.y += boid.vy;
-
-            // limit vel
-            // here <-----
             if (boid.vx > 6) {
                 boid.vx = 6;
             }
             if (boid.vy > 6) {
                 boid.vy = 6;
             }
+            if (boid.vx < 3) {
+                boid.vx = 3;
+            }
+            if (boid.vy < 3) {
+                boid.vy = 3;
+            }
+
+            boid.x = boid.x + boid.vx;
+            boid.y = boid.y + boid.vy;
+
             boid.shape.setPosition(boid.x, boid.y);
             // checking if boid within screen
             checkBorders(boid);
@@ -127,11 +150,10 @@ int main() {
 
         for(auto &boid : boids) {
             window.draw(boid.shape);
-            std::cout << boid.x << " " << boid.y << "\n";
+            //std::cout << boid.x << " " << boid.y << "\n";
         }
-        //std::this_thread::sleep_for(std::chrono::seconds(1));
         window.setVerticalSyncEnabled(true);
-        
+        //std::this_thread::sleep_for(std::chrono::seconds(1));
         window.display();
     }
 }
